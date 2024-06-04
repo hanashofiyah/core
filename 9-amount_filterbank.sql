81d291901ef0acc0a477c30a71c16849528b1488
hanashofiyah <104729134+hanashofiyah@users.noreply.github.com>
2024-06-04 22:07:16 +0700

    Delete 9-amount_filterbank.sql

--- 9-amount_filterbank.sql
+++
@@ -1,6 +0,0 @@
-SELECT bank.name AS bank_name, 
-       SUM(transaction.amount) AS total_amount_transfer
-FROM bank
-INNER JOIN transaction ON transaction.bank_id = bank.bank_code
-WHERE date_part('MONTH', transaction.transaction_date) = 5
-GROUP BY name;
